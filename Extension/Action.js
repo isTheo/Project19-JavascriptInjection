d96c932dc4323d868abcac57f3a9d39f4dfef2e1
isTheo <matteohg07@gmail.com>
2024-05-13 14:38:20 +0200

    Initial Commit

---
+++ Extension/Action.js
@@ -0,0 +1,17 @@
+//run is called before the extension is run and finalize after the code is run
+
+var Action = function() {};
+
+Action.prototype = {
+
+run: function(parameters) {
+
+},
+
+finalize: function(parameters) {
+
+}
+
+};
+
+var ExtensionPreprocessingJS = new Action
---
+++ Project19-JavascriptInjection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19-JavascriptInjection
+//
+//  Created by Matteo Orru on 13/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
